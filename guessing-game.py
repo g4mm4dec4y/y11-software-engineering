1438e40e381ab8e0b08387519c5a7648a61763c8
veronika <wawonika@gmail.com>
2025-09-23 13:41:58 +1000

    edited program logic

--- guessing-game.py
+++ guessing-game.py
@@ -1,41 +1,43 @@
+import random
 max_guess = 0
 min_guess = 1000
 
 #Function that generates a random number between 1 and 1000 inclusive
 #Returns that number
 def target_number():
-    random_number = random_number.randint(min_guess, max_guess)
+    random_number = random.randint(max_guess, min_guess)
     return random_number
 
 #Main game function
 def game_start():
     valid = True
 #Counter to keep track of how many guesses have been made
-    number_of_guesses =  0
+    number_of_guesses = 0
 #Assigns a random number
     game_number = target_number()
 #While loop keeps track ensuring only 10 guesses can be made
     while number_of_guesses <= 10:
-        win = True
         guess = input("Guess a number: ")
-        if guess != game_number:
+        if int(guess) != game_number:
         #Statements give hints as to where the number sits; above below
-            if guess > game_number:
+            if int(guess) > game_number:
                 print("Lower\n")
-            elif guess < game_number:
+            elif int(guess) < game_number:
                 print("Higher\n")
             number_of_guesses = number_of_guesses + 1
             #Counter gets added to for keeping track
-        else:
+        elif int(guess) == game_number:
         #If user wins, the while loop is broken to continue to regeneration
-            print("Correct! You win.")
+            print("Correct! You win.\n")
             break
-    #Message if lose
+        else:
+            print("Not a number.")
+     #Message if lose
     else:
         print("You lose.")
     
     #This segment asks the user if they want to restart
-    restart = input("Would you like to play again?(y/n) ")
+    restart = input("\nWould you like to play again?(y/n) ")
     while valid == True:
         if restart == "y":
             valid = False
